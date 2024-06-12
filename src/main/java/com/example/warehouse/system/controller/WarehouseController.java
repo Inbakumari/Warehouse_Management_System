57094cdbcd1a4cd8c106aeefc58c00574be99309
Inbauma <inbacse006@gmail.com>
2024-06-11 19:48:39 -0700

    Added updateAdmin by SuperAdmin

--- src/main/java/com/example/warehouse/system/controller/WarehouseController.java
+++ src/main/java/com/example/warehouse/system/controller/WarehouseController.java
@@ -3,6 +3,7 @@ package com.example.warehouse.system.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -38,4 +39,7 @@ public class WarehouseController {
 	{
 		return wareHouseService.updateWarehouse(warehouseRequest,warehouseId);
 	}
+	
+
+	
 }
